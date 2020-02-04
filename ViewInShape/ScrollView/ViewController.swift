e58d0e20a50b16661571cf60577168f80978730a
AlAbidAB <60339063+AlAbidAB@users.noreply.github.com>
2020-02-04 15:17:08 +0600

    Multiple shape using view.

---
+++ ViewInShape/ScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Training  on 12/12/19.
+//  Copyright © 2019 Training . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
